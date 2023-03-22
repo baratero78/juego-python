eb2ebdbce4f4b82db9f354819162324b07b628da
baratero78 <122360389+baratero78@users.noreply.github.com>
2023-03-22 08:50:16 -0300

    Add files via upload
    da4 version 22/3

--- da3.py
+++ da3.py
@@ -8,30 +8,36 @@ def lanzar_dados():
   dado2 = random.randint(1, 6)
   return dado1 + dado2  
   
+import os
+
 def opciones_numeros(disponibles, eliminados, puntos):
     while puntos > 0:
-        os.system('cls' if os.name == 'nt' else 'clear') #codigo para limpiar pantalla
+        os.system('cls' if os.name == 'nt' else 'clear') # Código para limpiar pantalla
         print(f"Tienes {puntos} puntos disponibles")
-        print(disponibles,"disponibles")
-        print(eliminados,"eliminados")
-        num = int(input("Ingresa el número que deseas eliminar o recuperar: "))
-        if num in disponibles and num<=puntos :
-            disponibles.remove(num)
-            eliminados.append(num)
-            puntos -= num
-        elif num in eliminados:
-            eliminados.remove(num)
-            disponibles.append(num)
-            puntos += num
-        else:
-            print("Ese número no está disponible ni eliminado")
+        print(f"Disponibles: {', '.join(map(str, sorted(disponibles)))}") # Presentar la lista de manera ordenada
+        print(f"Eliminados: {', '.join(map(str, sorted(eliminados)))}") # Presentar la lista de manera ordenada
+        try:
+            num = int(input("Ingresa el número que deseas eliminar o recuperar: "))
+            if num in disponibles and num <= puntos:
+                disponibles.remove(num)
+                eliminados.append(num)
+                puntos -= num
+            elif num in eliminados:
+                eliminados.remove(num)
+                disponibles.append(num)
+                puntos += num
+            else:
+                print("Número no válido")
+        except ValueError:
+            print("Por favor ingresa un número válido")
     return disponibles, eliminados, puntos
 
 
 
 
+
 def juego():
-    disponibles = [1, 2, 3, 4, 5,6,7,8,9]
+    disponibles = [1,2,3,4,5,6,7,8,9]
     eliminados = []
     print("Bienvenido a la numerala, el objetivo del juego es eliminar todos los números, recuerde que si la sumatoria de todos los números disponibles es menor que los puntos que obtuvo al tirar los dados el juego finalizara.")
     while disponibles:
